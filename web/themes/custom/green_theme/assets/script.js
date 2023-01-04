975961aecd4b2cbf6c765c430fe40aeec2d34601
Oleksandr Tymoshchuk <o.tymoshchuk@dev-branch.com>
2023-01-04 15:16:59 +0200

    5783.dev finished Home page & Fashion page styles

--- web/themes/custom/green_theme/assets/script.js
+++ web/themes/custom/green_theme/assets/script.js
@@ -9951,6 +9951,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _scss_styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/styles.scss */ "./src/scss/styles.scss");
 
 openSearchForm();
+scrollTop();
 function openSearchForm() {
   const searchBTN = document.querySelector('.search-block-form.block-search .search-form__submit');
   const searchQueryFields = document.querySelector('.search-block-form.block-search .form-search');
@@ -9960,6 +9961,33 @@ function openSearchForm() {
       searchQueryFields.classList.toggle('form-search--open');
     }
   });
+}
+
+//  Scroll Top Script
+function scrollTop() {
+  // Get the button:
+  let btn = document.getElementById("scroll-top-btn");
+  btn.addEventListener('click', () => {
+    topFunction();
+  });
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction(btn);
+  };
+}
+function scrollFunction(btn) {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 })();
 
--- web/themes/custom/green_theme/src/js/script.js
+++ web/themes/custom/green_theme/src/js/script.js
@@ -1,6 +1,8 @@
 import '../scss/styles.scss';
 
 openSearchForm();
+scrollTop();
+
 
 function openSearchForm() {
   const searchBTN = document.querySelector('.search-block-form.block-search .search-form__submit');
@@ -14,7 +16,35 @@ function openSearchForm() {
     }
 
   });
+}
 
+
+//  Scroll Top Script
+function scrollTop() {
+  // Get the button:
+  let btn = document.getElementById("scroll-top-btn");
+
+  btn.addEventListener('click', ()=>{
+    topFunction();
+  });
+
+// When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction(btn)
+  };
 
 }
 
+function scrollFunction(btn) {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
